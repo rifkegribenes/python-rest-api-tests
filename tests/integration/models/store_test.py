a9e59f8095be91a8ffcf69efb1d6e6558bf9c678
rifkegribenes <rifkegribenes@gmail.com>
2019-04-28 22:18:29 -0700

    stores tests

---
+++ tests/integration/models/store_test.py
@@ -0,0 +1,65 @@
+from models.store import StoreModel
+from models.item import ItemModel
+from tests.integration.integration_base_test import BaseTest
+
+
+class StoreTest(BaseTest):
+    def test_create_store_items_empty(self):
+        store = StoreModel('test')
+
+        self.assertListEqual(store.items.all(), [],
+                             "The stores' items length was not 0 even though no items were added.")
+
+    def test_crud(self):
+        with self.app_context():
+            store = StoreModel('test')
+            self.assertIsNone(StoreModel.find_by_name('test'),
+                              "Found a store with name {}, but expected not to.".format(store.name))
+
+            store.save_to_db()
+
+            self.assertIsNotNone(StoreModel.find_by_name('test'))
+
+            store.delete_from_db()
+
+            self.assertIsNone(StoreModel.find_by_name('test'))
+
+    def test_store_relationship(self):
+        with self.app_context():
+            store = StoreModel('test_store')
+            item = ItemModel('test_item', 19.99, 1)
+
+            store.save_to_db()
+            item.save_to_db()
+
+            self.assertEqual(store.items.count(), 1)
+            self.assertEqual(store.items.first().name, 'test_item')
+
+    def test_store_json(self):
+        store = StoreModel('test')
+        expected = {
+            'name': 'test',
+            'items': []
+        }
+
+        self.assertDictEqual(
+            store.json(),
+            expected,
+            "The JSON export of the store is incorrect. Received {}, expected {}.".format(store.json(), expected))
+
+    def test_store_json_with_item(self):
+        with self.app_context():
+            store = StoreModel('test')
+            item = ItemModel('test_item', 19.99, 1)
+            expected = {
+                'name': 'test',
+                'items': [{'name': 'test_item', 'price': 19.99}]
+            }
+
+            store.save_to_db()
+            item.save_to_db()
+
+            self.assertDictEqual(
+                store.json(),
+                expected,
+                "The JSON export of the store is incorrect. Received {}, expected {}.".format(store.json(), expected))
